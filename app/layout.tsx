52197428c63e11e02c69a1801c92b8bf0fdf09d5
Warui Denis <33454398+Deniswarui4@users.noreply.github.com>
2025-08-26 13:17:07 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Mpaas',
+  title: 'Mpaas ~> STK Push made easy',
   description: 'STK Push made easy',
 }
 
